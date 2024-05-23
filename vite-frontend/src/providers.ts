f2e7c2d1b5434d07b3064660eea5cf006f97458f
Byron Thomas <byron.thomas@gmail.com>
2024-05-23 13:43:15 +0100

    Last attempts to get going with vanilla TS

--- vite-frontend/src/providers.ts
+++ vite-frontend/src/providers.ts
@@ -4,23 +4,38 @@ declare global {
   }
 }
 
+export type ConnectedState = {
+  provider: EIP1193Provider;
+  info: EIP6963ProviderInfo;
+  chainId: number;
+};
+
 export type ConnectionState = {
   isConnected: boolean;
-  connectedState?: {
-    provider: EIP1193Provider;
-    info: EIP6963ProviderInfo;
-  };
+  connectedState?: ConnectedState;
   connectionError?: string;
 };
 
-// Connect to the selected provider using eth_requestAccounts.
+// Connect to the selected provider using eth_requestAccounts and
+// check the currently connected chain.
 const connectWithProvider = async (
   wallet: EIP6963AnnounceProviderEvent["detail"],
   onStateUpdated: (newConnState: ConnectionState) => void
 ) => {
   try {
     await wallet.provider.request({ method: "eth_requestAccounts" });
-    onStateUpdated({ isConnected: true, connectedState: wallet });
+    const connState: ConnectedState = {
+      provider: wallet.provider,
+      info: wallet.info,
+      chainId: -1,
+    };
+    const updatedState: ConnectionState = {
+      isConnected: true,
+      connectedState: connState,
+    };
+    const chainId = await detectNetwork(wallet.provider);
+    connState.chainId = chainId;
+    onStateUpdated(updatedState);
   } catch (error: any) {
     console.error("Failed to connect to provider:", error);
     onStateUpdated({ isConnected: false, connectionError: error.message });
@@ -51,6 +66,55 @@ export function listProviders(
 
   // Notify event listeners and other parts of the dapp that a provider is requested.
   window.dispatchEvent(new Event("eip6963:requestProvider"));
+}
+
+export async function detectNetwork(provider: EIP1193Provider) {
+  const chainId = await provider // Or window.ethereum if you don't support EIP-6963.
+    .request({ method: "eth_chainId" });
+  console.log("Currently on chain", chainId);
+  // chainId should be a string that is either in hex or decimal format.
+  // return it as an integer. If it is in hex it should start 0x, otherwise
+  // it should be a decimal number.
+  return parseChainId(chainId);
+}
+
+export function parseChainId(chainId: unknown) {
+  return parseInt(String(chainId));
+}
+
+export function formatChainId(chainId: number) {
+  return `0x${chainId.toString(16)}`;
+}
+
+export async function switchNetwork(
+  initialState: ConnectionState,
+  newChainId: string,
+  onChainSwitched: (appState: ConnectionState) => void,
+  onError: (msg: string) => void
+) {
+  try {
+    const connectedState = initialState.connectedState!;
+    await connectedState.provider.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: newChainId }],
+    });
+    onChainSwitched({
+      isConnected: true,
+      connectedState: {
+        ...connectedState,
+        chainId: parseChainId(newChainId),
+      },
+    });
+  } catch (switchError: any) {
+    // This error code indicates that the chain has not been added to MetaMask.
+    if (switchError.code === 4902) {
+      onError(
+        "Network doesn't exist. Please add the network details for it to your wallet provider (e.g. MetaMask)"
+      );
+    } else {
+      onError(switchError.message);
+    }
+  }
 }
 
 console.log("Got a location as", window.location.href);
